90e599c9bf61adce5786833c109ca66c43a2a60a
mr-subroutine <darrylshack@outlook.com>
2020-05-07 16:53:54 -0700

    added functionality for calculating area and temperature conversion

--- UsefulCalculator1/Form1.Designer.cs
+++ UsefulCalculator1/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtTable = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate Area of Square";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Calculate Area of Circle";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(24, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 37);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Celcius to Fahrenheit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtTable
+            // 
+            this.txtTable.Location = new System.Drawing.Point(271, 13);
+            this.txtTable.Multiline = true;
+            this.txtTable.Name = "txtTable";
+            this.txtTable.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtTable.Size = new System.Drawing.Size(162, 174);
+            this.txtTable.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(512, 450);
+            this.Controls.Add(this.txtTable);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Useful Calculator 1 v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtTable;
     }
 }
 
--- UsefulCalculator1/Form1.cs
+++ UsefulCalculator1/Form1.cs
@@ -12,9 +12,82 @@ namespace UsefulCalculator1
 {
     public partial class Form1 : Form
     {
+        protected int value;
+        protected int celcius;
+        protected int lengthAndWidthCount = 0;
+        protected int length;
+        protected int width;
+        protected int fdegrees;
+        protected int cdegrees;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lengthAndWidthCount = 1;
+            length = 0;
+            width = 0;
+
+            while (lengthAndWidthCount < 3)
+            {
+                try
+                { 
+                    value = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter your value: ",
+                        "First Value = LENGTH, Second Value = WIDTH"));
+                }
+
+                catch (Exception)
+                {
+                    MessageBox.Show("Enter a correct numerical value");
+                }
+
+                if (lengthAndWidthCount == 1)
+                {
+                    length += value;
+                }
+
+                if (lengthAndWidthCount == 2)
+                {
+                    width += value;
+                }
+                value = 0;
+                lengthAndWidthCount++;
+            }
+
+            value = length * width;
+            lengthAndWidthCount = 1;
+            MessageBox.Show("The area is: " + value, "ANSWER", MessageBoxButtons.OK, MessageBoxIcon.Question);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            celcius = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter a degree in celcius: ",
+                       "Convert Celsius to Fahrenheit"));
+            celcius = convertToF(celcius);
+            MessageBox.Show("The temperature in F is:  " + celcius, "Fahrenheit", MessageBoxButtons.OK, MessageBoxIcon.Question);
+
+            int newCelcius = 0;
+            int convertedValue;
+            txtTable.Text = "Celsius\t Fahrenheit" + Environment.NewLine;
+
+            while (newCelcius < 52)
+            {
+                convertedValue = convertToF(newCelcius);
+                txtTable.Text += newCelcius.ToString() + "\t" + convertedValue.ToString() + Environment.NewLine;
+                //celciusCounter++;
+                newCelcius++;
+            }
+        }
+
+        private int convertToF(int cdegrees)
+        {
+            int result1 = Convert.ToInt32(cdegrees * 1.8);
+            result1 = result1 + 32;
+            return result1;
         }
     }
 }
